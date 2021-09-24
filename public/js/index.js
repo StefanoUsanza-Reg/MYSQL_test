c505c777945ddce8c731781d6c3264dfc4b8ffad
Stefano Usanza <stefano.usanza@regestaitalia.it>
2021-09-24 17:55:40 +0200

    commit

--- public/js/index.js
+++ public/js/index.js
@@ -12,32 +12,3 @@ btnRicerca.onclick = ()=>{
     console.log(data)
 
 }
-
-//ricerca nel database i dati da inserire nel campo nome_utente
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "restock"
-});
-
-let QUERY = "SELECT prodotto.nome as nomeProdotto, rivende.prezzo, rivende.quantità, rivenditore.nome as nomeRivenditore, rivenditore.spedizione_min, "+ 
-"sconto.valore, sconto_extra.valore as valore_extra, sconto.importo_minimo, sconto.quantità_min, sconto_extra.data_inizio, sconto_extra.data_fine "+
-"FROM prodotto, rivende, rivenditore, sconto, sconto_extra "+
-"WHERE id_prodotto=prodotto.id "+
-"and id_rivenditore=rivenditore.id "+
-"and id_sconto = sconto.id "+
-"and id_sconto_extra = sconto_extra.id "+
-"and prodotto.nome='"
-
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("connected to the database!")
-con.query(QUERY+ nome_prodotto+"' and rivende.quantità>="+quant, function (err, result, fields) {
-    if (err) throw err;
-    console.log(result)
-});
-con.end()
-})
