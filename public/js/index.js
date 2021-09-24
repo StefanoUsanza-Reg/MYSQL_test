042f183584cfbe5df2b247f4bd881c020b169b7b
Stefano Usanza <stefano.usanza@regestaitalia.it>
2021-09-24 16:32:17 +0200

    add front-end

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+const btnRicerca = document.getElementById('ricerca')
+
+btnRicerca.onclick = ()=>{
+    const nome_prodotto = document.getElementById('nome_prodotto').value
+    const quant = document.getElementById('quant').value
+    const priority = document.getElementById('priority').value
+    const data = new Date(Date.now())
+
+    console.log(nome_prodotto)
+    console.log(quant)
+    console.log(priority)
+    console.log(data)
+
+}
+
+
