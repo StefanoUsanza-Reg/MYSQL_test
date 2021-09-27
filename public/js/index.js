59f62b252ad84b496d452053d6e053c6455e575d
Stefano Usanza <stefano.usanza@regestaitalia.it>
2021-09-27 12:32:11 +0200

    implementazione funzioni restock

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,147 @@
 const btnRicerca = document.getElementById('ricerca')
 
+    function sconto(numero,percentuale){
+        return numero - (numero * percentuale)/100
+    }
+
+function restock(result,quant,data){
+
+    //controllo validità sconti
+    for(let i=0; i<result.length; i++){
+        if(result[i].quantità_min>quant || result[i].importo_minimo>(result[i].prezzo*quant))
+            result[i].valore = 0
+    }
+    //applicazione sconti
+    var prezzo_scontato = sconto(sconto(result[0].prezzo*quant,result[0].valore),result[0].valore_extra)
+    var rivenditore = result[0].nomeRivenditore
+    var spedizione = result[0].spedizione_min
+    console.log("il rivenditore "+ rivenditore +" può accettare la richiesta con un prezzo finale di: "+ prezzo_scontato+"€ , spedizione minima: " + spedizione + " giorni")
+    var tempScontato
+    var tempSpedizione
+    for(let i=1; i<result.length;i++){
+        //sconto extra valido
+        if(result[i].data_inizio<=data && result[i].data_fine>=data){
+            tempScontato = sconto(sconto(result[i].prezzo*quant,result[i].valore),result[i].valore_extra)
+            tempScontato = Math.round((tempScontato + Number.EPSILON) * 100) / 100;
+            tempSpedizione = result[i].spedizione_min
+            console.log("il rivenditore " + result[i].nomeRivenditore + " può accettare la richiesta con un prezzo finale di: "+ tempScontato + "€, spedizione minima: " + tempSpedizione + " giorni")
+            if(tempScontato<prezzo_scontato){
+                prezzo_scontato = tempScontato
+                rivenditore = result[i].nomeRivenditore
+                spedizione = tempSpedizione
+            }
+            else if(tempScontato==prezzo_scontato){
+                if(tempSpedizione<spedizione){
+                    rivenditore = result[i].nomeRivenditore
+                    spedizione = tempSpedizione 
+                }
+            }                     
+        }           
+        //sconto extra non valido
+        else{
+            tempScontato = sconto(result[i].prezzo*quant,result[i].valore)
+            tempScontato = Math.round((tempScontato + Number.EPSILON) * 100) / 100;
+            tempSpedizione = result[i].spedizione_min
+            console.log("il rivenditore " + result[i].nomeRivenditore + " può accettare la richiesta con un prezzo finale di: "+ tempScontato + "€, spedizione minima: " + tempSpedizione + " giorni")
+            if(tempScontato<prezzo_scontato){
+                prezzo_scontato = tempScontato
+                rivenditore = result[i].nomeRivenditore
+                spedizione = tempSpedizione
+            }
+            else if(tempScontato==prezzo_scontato){
+                if(tempSpedizione<spedizione){
+                    rivenditore = result[i].nomeRivenditore
+                    spedizione = tempSpedizione 
+                }
+            }                    
+        }           
+    }
+    //evidenzia il rivenditore più conveniente
+    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    var migliore = "il rivenditore " + rivenditore + " è il più conveniente con un prezzo finale di: "+ prezzo_scontato + "€, spedizione minima: " + spedizione + " giorni"
+    return migliore
+}
+
+function fastRestock(result,quant,data){
+    //controllo validità sconti
+    for(let i=0; i<result.length; i++){
+        if(result[i].quantità_min>quant || result[i].importo_minimo>(result[i].prezzo*quant))
+            result[i].valore = 0
+    }
+    //applicazione sconti
+    var prezzo_scontato = sconto(sconto(result[0].prezzo*quant,result[0].valore),result[0].valore_extra)
+    var rivenditore = result[0].nomeRivenditore
+    var spedizione = result[0].spedizione_min
+    console.log("il rivenditore "+ rivenditore +" può accettare la richiesta con un prezzo finale di: "+ prezzo_scontato+"€ , spedizione minima: " + spedizione + " giorni")
+    var tempScontato
+    var tempSpedizione
+    for(let i=1; i<result.length;i++){
+        //sconto extra valido
+        if(result[i].data_inizio<=data && result[i].data_fine>=data){
+            tempScontato = sconto(sconto(result[i].prezzo*quant,result[i].valore),result[i].valore_extra)
+            tempScontato = Math.round((tempScontato + Number.EPSILON) * 100) / 100;
+            tempSpedizione = result[i].spedizione_min
+            console.log("il rivenditore " + result[i].nomeRivenditore + " può accettare la richiesta con un prezzo finale di: "+ tempScontato + "€, spedizione minima: " + tempSpedizione + " giorni")
+            if(tempSpedizione<spedizione){
+                prezzo_scontato = tempScontato
+                rivenditore = result[i].nomeRivenditore
+                spedizione = tempSpedizione
+            }
+            else if(tempSpedizione==spedizione){
+                if(tempScontato<prezzo_scontato){
+                    prezzo_scontato = tempScontato
+                    rivenditore = result[i].nomeRivenditore
+                }
+            }                     
+        }           
+        //sconto extra non valido
+        else{
+            tempScontato = sconto(result[i].prezzo*quant,result[i].valore)
+            tempScontato = Math.round((tempScontato + Number.EPSILON) * 100) / 100;
+            tempSpedizione = result[i].spedizione_min
+            console.log("il rivenditore " + result[i].nomeRivenditore + " può accettare la richiesta con un prezzo finale di: "+ tempScontato + "€, spedizione minima: " + tempSpedizione + " giorni")
+            if(tempSpedizione<spedizione){
+                prezzo_scontato = tempScontato
+                rivenditore = result[i].nomeRivenditore
+                spedizione = tempSpedizione
+            }
+            else if(tempSpedizione==spedizione){
+                if(tempScontato<prezzo_scontato){
+                    prezzo_scontato = tempScontato
+                    rivenditore = result[i].nomeRivenditore
+                }
+            }                    
+        }           
+    }
+    //evidenzia il rivenditore più conveniente
+    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    var migliore = "il rivenditore " + rivenditore + " è il più veloce consegnando in: "+ spedizione+" giorni, con un prezzo finale di: "+ prezzo_scontato + "€"
+    return migliore
+}
+
 btnRicerca.onclick = ()=>{
     const nome_prodotto = document.getElementById('nome_prodotto').value
     const quant = document.getElementById('quant').value
     const priority = document.getElementById('priority').value
     const data = new Date(Date.now())
 
-    console.log(nome_prodotto)
-    console.log(quant)
-    console.log(priority)
-    console.log(data)
+    //fetch("http://localhost:3000/restock/economic/"+nome_prodotto+"/"+quant).then(() => console.log("test"));
+
+    //ricerca di tutti i clienti nel database
+fetch('http://localhost:3000/restock/'+nome_prodotto+'/'+quant)
+.then(response => response.json())
+  .then(result => {
+      if(result[0]!=null){
+        if(priority=="Economic")
+        console.log(restock(result,quant,data))
+        else if(priority=='Fast')
+        console.log(fastRestock(result,quant,data))    
+      }
+      else
+        console.log("nessun rivenditore trovato")
+      
+    });
+
+
 
 }
--- server/server.js
+++ server/server.js
@@ -8,7 +8,43 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const assert = require('chai').assert
+const expect = require('chai').expect
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "restock"
+});
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("connected to the database!")
+})
+let QUERY = "SELECT prodotto.nome as nomeProdotto, rivende.prezzo, rivende.quantità, rivenditore.nome as nomeRivenditore, rivenditore.spedizione_min, "+ 
+"sconto.valore, sconto_extra.valore as valore_extra, sconto.importo_minimo, sconto.quantità_min, sconto_extra.data_inizio, sconto_extra.data_fine "+
+"FROM prodotto, rivende, rivenditore, sconto, sconto_extra "+
+"WHERE id_prodotto=prodotto.id "+
+"and id_rivenditore=rivenditore.id "+
+"and id_sconto = sconto.id "+
+"and id_sconto_extra = sconto_extra.id "+
+"and prodotto.nome='"
+
+//server express in ascolto sulla porta 3000
 app.listen(3000, ()=>{
     console.log("server listening on port 3000:")
 })
 
+app.get('/restock/:nome_prodotto/:quant',(req,res)=>{
+    const {nome_prodotto} = req.params
+    const {quant} = req.params
+
+    con.query(QUERY+ nome_prodotto+"' and rivende.quantità>="+quant, function (err, result, fields) {
+        if (err) throw err;
+        //con.end()
+        res.send(result)
+    });
+
+})
