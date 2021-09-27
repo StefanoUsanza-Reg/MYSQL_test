4214f74a001521234a081ff581c13773a6c7c44b
Stefano Usanza <stefano.usanza@regestaitalia.it>
2021-09-27 09:08:17 +0200

    add server express

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app= express()
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(3000, ()=>{
+    console.log("server listening on port 3000:")
+})
+
